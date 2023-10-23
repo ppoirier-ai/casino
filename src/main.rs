fae9fd5dfd8611a16739bb9d2578f4480e6544ef
Crypto Rookies <patrick@halcyondao.finance>
2023-10-23 11:28:39 +0800

    working program using a struct

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,20 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn main() {
-    println!("Guess the number!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!(
+        "The area of the rectange is {} square pixels.",
+        area(&rect1)
+    );
+}
 
-    //println!("The secret number is : {secret_number}");
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Small!"),
-            Ordering::Greater => println!("Too Big!"),
-            Ordering::Equal => {
-                println!("You Won!");
-                break;
-            }
-        }
-    }
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
