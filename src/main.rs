84c197acfe63d463c7cc20d671deea8ce3f4f3ac
Crypto Rookies <patrick@halcyondao.finance>
2023-11-16 11:11:21 +0800

    struggling with proper 2-way interactions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io;
-use slotmachine::spin;
+//use slotmachine::spin;
 use teloxide::{dispatching::dialogue::InMemStorage, prelude::*};
 use tokio;
 
@@ -10,7 +10,7 @@ type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 pub enum State {
     #[default]
     Telegram,
-    Receive_selection {
+    receive_selection {
         rec_selection: String,
     },
 }
@@ -20,13 +20,13 @@ async fn main() {
 
     println!("Welcome to our Crypto Slot Machine!!!");
     let bot = Bot::from_env(); // import telegram bot token from server environment variable
-
+    //println!("the bot token is {}", bot.token());
     Dispatcher::builder(
         bot,
         Update::filter_message()
             .enter_dialogue::<Message, InMemStorage<State>, State>()
             .branch(dptree::case![State::Telegram].endpoint(telegram))
-            .branch(dptree::case![State::Receive_selection { rec_selection }].endpoint(receive_selection)),
+            .branch(dptree::case![State::receive_selection { rec_selection }].endpoint(receive_selection)),
     )
     .dependencies(dptree::deps![InMemStorage::<State>::new()])
     .enable_ctrlc_handler()
@@ -51,7 +51,7 @@ async fn main() {
         println!("You chose: {selection}");
 
         match selection {
-            1 => { slotmachine::spin(); let t1 = test(); let t2 = test(); tokio::join!(t1,t2); },
+            1 => { println!("testing selection 1"); slotmachine::spin(); let t1 = test(); let t2 = test(); tokio::join!(t1,t2); },
             3 => { println!("You selected to cash out"); },
             4 => { println!("You selected to deposit"); },
             2 => { println!("Good bye and see you soon!"); break; },
@@ -65,14 +65,19 @@ async fn test() { println!("First Task"); }
 async fn telegram(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
     //pretty_env_logger::init();
     //log::info!("Starting throw dice bot...");
-
+    println!("starting the telegram function");
+    bot.send_message(msg.chat.id, "What is your selection?").await?;
     match msg.text() {
-        Some(text) => {
-            println!("testing telegram bot");
-            bot.send_message(msg.chat.id, "What is your selection?").await?;
-            dialogue.update(State::Receive_selection { rec_selection: text.into() }).await?;
-        }
+        Some("1") => {
+            println!("testing telegram bot {}", msg.chat.id.to_string());
+            
+            dialogue.update(State::receive_selection { rec_selection: "1".into() });
+            println!("finishing telegram function with");
+        },
+        Some("2") => { println!("you selected option 2"); },
+        Some(text) => { println!("did not match anyting specific"); },
         None => {
+            println!("I do not think this is working");
             bot.send_message(msg.chat.id, "Send me plain text").await?;
         }
     }
@@ -80,12 +85,15 @@ async fn telegram(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult
 }
 
 async fn receive_selection(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    println!("starting the receive_selection function");
     match msg.text() {
         Some(text) => {
+            println!("will the receive function work?");
             bot.send_message(msg.chat.id, "tell me your name").await?;
-            //dialogue.update(State::Receive_selection { rec_selection: text.into() }).await?;
+            dialogue.update(State::receive_selection { rec_selection: text.into() }).await?;
         }
         None => {
+            println!("nope, the receive function is not working");
             bot.send_message(msg.chat.id, "Send me plain text").await?;
         }
     }
